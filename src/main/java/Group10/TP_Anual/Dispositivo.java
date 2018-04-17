10706b87a5691e6ece7ea0d6f0b6d0ca109bd2f8
Gonzalo Bertinat <gonzalo_bertinat_98@hotmail.com>
2018-04-16 23:05:35 -0300

    Creando proyecto Maven + Clase Dispositivo

---
+++ src/main/java/Group10/TP_Anual/Dispositivo.java
@@ -0,0 +1,30 @@
+package Group10.TP_Anual;
+
+public class Dispositivo {
+
+	private String nombreGenerico;
+	private double kWxHora;
+	private boolean encendido;
+	
+	// DUDO DEL CONSTRUCTOR, CONSULTAR AL PROFE SI ES NECESARIO!
+	public Dispositivo(String nombre, double kW)
+	{
+		this.nombreGenerico = nombre;
+		this.kWxHora = kW;
+		this.encendido = false; // POR DEFECTO un dispositivo está apagado
+	}
+	
+	// GETTERS PEDIDOS EN ENTREGA 0
+	public String nombreGenerico()
+	{
+		return nombreGenerico;
+	}
+	public double kwConsumoxHora()
+	{
+		return kWxHora;
+	}
+	public boolean encendido()
+	{
+		return encendido;
+	}
+}
