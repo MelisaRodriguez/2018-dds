22d4952d97406a4af34f3cc2f32422c5fb7a31ce
unknown <facundofraguaga@hotmail.es>
2018-05-24 21:41:19 -0300

    creada la clase condicion y modificado el sensor

--- src/main/java/edu/actuadoresSensores/Condicion.java
+++ src/main/java/edu/actuadoresSensores/Condicion.java
@@ -1,5 +1,19 @@
 package edu.actuadoresSensores;
 
-public class Condicion {
+import java.util.function.Function;
 
+public class Condicion {
+	public Sensor sensor; 
+	public Function<Double,Boolean> condicionLogica;
+	
+	public Condicion(Sensor sensor, Function<Double, Boolean> condicion) {
+		this.sensor = sensor;
+		this.condicionLogica = condicion;
+	}
+
+	public boolean medicionCumpleCondicion () {
+		return condicionLogica.apply(sensor.getMedida());
+	}
+	
 }
+
