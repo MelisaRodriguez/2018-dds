f153e329d12b6261803f14fae156c386c2c71a80
Luci <facundo.lucero@outlook.es>
2018-11-25 22:36:01 -0300

    Meliza Rodriguez ingeniera, heroe de guerra, doctora, sexologa,alquimista

--- src/main/java/main/server/Server.java
+++ src/main/java/main/server/Server.java
@@ -3,15 +3,45 @@ package main.server;
 import spark.Spark;
 import spark.debug.DebugScreen;
 
+
+import java.io.IOException;
+
+import java.io.File;
+import java.io.FileWriter;
+
+
 public class Server {
+
 	public static void main(String[] args) {
-		Bootstrap.init();
-
-		DebugScreen.enableDebugScreen();
-		
-		Spark.port(4444);
-
-		Router.configure();
-
+		File archivo=new File("./Logs.log");			
+		if(archivo.exists()) 
+		{
+			try 
+			{
+				int a=1/0;
+				Bootstrap.init();
+				DebugScreen.enableDebugScreen();
+				Spark.port(4444);
+				Router.configure();
+			}
+			catch(Exception e) {
+				escribirLog("No se ha podido iniciar el servidor:\n"+e.getStackTrace().toString());
+			}
+		}
+		else {
+			System.out.println("No existe archivo");
+		}
 	}
+	
+	public static void escribirLog(String cadena) {
+		try {
+			FileWriter fw=new FileWriter("./Logs.log",true);
+			fw.write(cadena+"\r\n");
+			fw.close();
+		}
+		catch(IOException x) {
+			x.printStackTrace();
+		}
+	}
+	
 }
